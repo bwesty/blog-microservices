d5ac91c838925ff6bab29bddeda9f90c0edbc4b8
Brian Westy Rasmussen <b.westy@me.com>
2023-04-22 11:57:35 +0200

    hmm

--- microservices/api/product-api-service/src/main/java/se/callista/microservices/api/product/ProductApiServiceApplication.java
+++ microservices/api/product-api-service/src/main/java/se/callista/microservices/api/product/ProductApiServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
 
+
+
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableDiscoveryClient
